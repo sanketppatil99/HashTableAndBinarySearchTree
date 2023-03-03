3a1d392813bad5316f304bfa8c226ed4bc48b2bb
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 03:46:06 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HashTableAndBST
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table and BST");
+
+        }
+    }
+}
