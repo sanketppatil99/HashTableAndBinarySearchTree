12d3f62b63785729523e8c0660ee21c27fd4c453
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 03:49:29 +0530

    Ability to find frequency of word in a sentence

--- Program.cs
+++ Program.cs
@@ -5,7 +5,18 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table and BST");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
+            hash.Add("0", "to");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            string hash3 = hash.Get("3");
 
+            Console.WriteLine("Value of key 5 is :" + " " + hash5);
+            Console.WriteLine("Value of key 2 is :" + " " + hash3);
         }
     }
 }
