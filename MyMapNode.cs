0b9f38d44ae8cb0ec28b02440af06561c2ed0cde
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 03:50:31 +0530

    Ability to find frequency of word in a sentence

---
+++ MyMapNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBST
+{
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> items in linkedlist)
+            {
+                if (items.Key.Equals(key))
+                {
+                    return items.Value;
+                }
+            }
+            return default(V);
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linklist = GetLinkedList(position);
+            KeyValue<K, V> items = new KeyValue<K, V> { Key = key, Value = value };
+            linklist.AddLast(items);
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+    }
+}
