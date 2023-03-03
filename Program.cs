0a9f2947a806f42aa0b733f3bae0bc6c5581cdc7
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 03:56:20 +0530

    Ability to find frequency of word in a large paragraph phrase

--- Program.cs
+++ Program.cs
@@ -6,17 +6,33 @@
         {
             Console.WriteLine("Welcome to Hash Table and BST");
             MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
-            hash.Add("0", "to");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
+            hash.Add("0", "Paranoid");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "themselves");
+            hash.Add("13", "deleberately");
+            hash.Add("14", "into");
+            hash.Add("15", "paranoid");
+            hash.Add("5", "avoidable");
+            hash.Add("5", "situations");
             string hash5 = hash.Get("5");
             string hash3 = hash.Get("3");
+            string hash12 = hash.Get("12");
+            string hash8 = hash.Get("8");
 
             Console.WriteLine("Value of key 5 is :" + " " + hash5);
-            Console.WriteLine("Value of key 2 is :" + " " + hash3);
+            Console.WriteLine("Value of key 3 is :" + " " + hash3);
+            Console.WriteLine("Value of key 12 is :" + " " + hash12);
+            Console.WriteLine("Value of key 8 is :" + " " + hash8);
         }
     }
 }
